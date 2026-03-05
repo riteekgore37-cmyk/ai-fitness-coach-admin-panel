f3764989e68c4d19144101c1ff37b9ad694d7f85
riteek <riteekgore37@gmail.com>
2026-03-05 14:38:39 +0530

    Converted admins page to client component

--- src/app/(routes)/admins/page.tsx
+++ src/app/(routes)/admins/page.tsx
@@ -1,20 +1,46 @@
-import { getAdmins } from '@/app/Actions/GetActions'
-import DataViewTable from '@/components/large/DataViewTable/DataViewTable'
-import React from 'react'
+"use client";
 
+import { useEffect, useState } from "react";
+import DataViewTable from "@/components/large/DataViewTable/DataViewTable";
 
+export default function Admins() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-async function Admins() {
-    const {data} = await getAdmins();
-    console.log('admins',data)
-    return (
-        <div>
-            <DataViewTable
-             data={data}
-             keysToDisplay={['id','name','email','role','gender','age']} 
-            />
-        </div>
-    )
+  useEffect(() => {
+    const fetchAdmins = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/console/admins`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const result = await res.json();
+        setData(result.data);
+      } catch (error) {
+        console.error("Error fetching admins:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAdmins();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <DataViewTable
+        data={data}
+        keysToDisplay={["id", "name", "email", "role", "gender", "age"]}
+      />
+    </div>
+  );
 }
-
-export default Admins
