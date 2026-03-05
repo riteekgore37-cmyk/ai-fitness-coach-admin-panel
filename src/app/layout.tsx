20b4463a4072951c4df11f8824a3ce22fcaed927
riteek <riteekgore37@gmail.com>
2026-03-05 14:43:37 +0530

    Disabled static prerendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React, { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -23,7 +25,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={inter.className}>
         <main className="mainStructure">
           <div className="sidedrawer">
